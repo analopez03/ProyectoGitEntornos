96fa40bbff48c24fdd38ef849105cde975603cc6
analopez03 <analopezdeahumada97@gmail.com>
2024-02-29 13:11:46 +0100

    PasoSexto

--- Archivo3.java
+++ Archivo3.java
@@ -3,4 +3,11 @@ package com.entorno;
 public class Archivo3 {
 	private String Guarana;
 	private String Piña;
+	
+	public Archivo3() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
